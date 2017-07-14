ceed559f57aaaed7b2c704a47f39cd5ba7f8ad7f
Maxim Smirnov <devnull@localhost>
2017-07-14 11:12:52 +0300

    в название дистрибутива добавлен префикс helpme

--- build.cmd
+++ build.cmd
@@ -8,5 +8,5 @@ mkdir PnFMods
 xcopy ..\..\HelpMe PnFMods\HelpMe /i /e
 echo dir=%3 >PnFMods\HelpMe\helpme.ini
 cd ..
-"C:\Program Files\7-Zip\7z.exe" a -r C:\src\owncloud\HelpMe\%1-%2-%3.zip %1
+"C:\Program Files\7-Zip\7z.exe" a -r C:\src\owncloud\HelpMe\helpme-%1-%2-%3.zip %1
 cd ..
