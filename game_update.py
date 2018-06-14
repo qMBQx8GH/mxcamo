8a05b6233dd242d574bd6d4b9b985abaafe9edff
Maxim Smirnov <devnull@localhost>
2018-06-14 09:33:48 +0200

    учтена ситуация когда нет обновлений кроме версии игры и убрал тегирование

--- game_update.py
+++ game_update.py
@@ -22,20 +22,23 @@ for path in xml.iter('Path'):
 print('res_mods: {}'.format(res_mods))
 
 if version != res_mods:
-    res = subprocess.run(['hg', 'merge', 'default', '--tool', 'internal:other'])
-    if res.returncode != 0:
-      exit(res.returncode)
-    res = subprocess.run(['hg', 'com', '-v', '-m', 'merge'])
-    if res.returncode != 0:
+    res = subprocess.run(['hg', 'merge', 'default', '--tool', 'internal:other'], stderr=subprocess.PIPE)
+    print(res.returncode, res.stderr.decode())
+
+    if res.returncode == 0:
+        res = subprocess.run(['hg', 'com', '-v', '-m', 'merge'])
+        if res.returncode != 0:
+          exit(res.returncode)
+    elif res.stderr.decode().startswith('abort: merging with a working directory ancestor has no effect'):
+        pass
+    else:
+      print(res.stderr.decode())
       exit(res.returncode)
 
     f = open('version.txt', 'w')
     f.write(res_mods)
     f.close()
     res = subprocess.run(['hg', 'com', '-v', '-m', res_mods, 'version.txt'])
-    if res.returncode != 0:
-      exit(res.returncode)
-    res = subprocess.run(['hg', 'tag', res_mods])
     if res.returncode != 0:
       exit(res.returncode)
     
