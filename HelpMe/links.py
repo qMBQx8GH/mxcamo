386640ecf8e9fbfe446eb49d052da4bf12bcdbb0
Maxim Smirnov <devnull@localhost>
2018-10-31 09:30:19 +0200

    исправлена ошибка приводившая к отключеию мода

--- HelpMe/links.py
+++ HelpMe/links.py
@@ -10,17 +10,18 @@ class MyLinks:
     def createFlashLinks(self, ship_id):
         if ship_id in self._link_menus:
             return
-        print self._links
         for link in self._links:
             if link['ship_id'] == ship_id:
-                if ship_id not in self._link_menus:
+                if ship_id not in self._link_menus \
+                        and 'ship_name' in link:
                     flash.call('HelpMe.CREATE_MENU', [ship_id, link['ship_name']])
                     self._link_menus[ship_id] = {}
-                if link['url'] not in self._link_menus[ship_id]:
+                if ship_id in self._link_menus \
+                        and link['url'] not in self._link_menus[ship_id] \
+                        and 'title' in link:
                     i = len(self._link_menus[ship_id]) + 1
                     title = link['title']
                     id = ship_id + '[' + str(i) + ']'
                     flash.call('HelpMe.ADD_MENU_ITEM', [ship_id, title, id])
                     flash.call('HelpMe.CREATE_QR_CODE', [id, 510, 100, link['url']])
                     self._link_menus[ship_id][link['url']] = id
-        print self._link_menus
