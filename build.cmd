dc3a4a974c70d0afb03a0ac62137a9f14e3712fb
Maxim Smirnov <devnull@localhost>
2019-03-06 09:04:13 +0200

    некоторые параметры автосборки вынесены в ini файл

--- build.cmd
+++ build.cmd
@@ -1,5 +1,46 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
+SET GAME_FOLDER=
+SET INI=build.ini
+set area=[Game]
+set key=folder
+set currarea=
+for /f "usebackq delims=" %%a in ("!INI!") do (
+    set ln=%%a
+    if "x!ln:~0,1!"=="x[" (
+        set currarea=!ln!
+    ) else (
+        for /f "tokens=1,2 delims==" %%b in ("!ln!") do (
+            set currkey=%%b
+            set currval=%%c
+            if "x!area!"=="x!currarea!" if "x!key!"=="x!currkey!" (
+                set GAME_FOLDER=%%c
+            )
+        )
+    )
+)
+echo %GAME_FOLDER%
+
+set DESTINATION=
+set area=[Destination]
+set key=folder
+set currarea=
+for /f "usebackq delims=" %%a in ("!INI!") do (
+    set ln=%%a
+    if "x!ln:~0,1!"=="x[" (
+        set currarea=!ln!
+    ) else (
+        for /f "tokens=1,2 delims==" %%b in ("!ln!") do (
+            set currkey=%%b
+            set currval=%%c
+            if "x!area!"=="x!currarea!" if "x!key!"=="x!currkey!" (
+                set DESTINATION=%%c
+            )
+        )
+    )
+)
+echo %GAME_FOLDER%
+
 SET REV=
 for /f "tokens=*" %%i in ('hg parent --template "{rev}"') do (
   set REV=%%i
@@ -7,7 +48,7 @@ for /f "tokens=*" %%i in ('hg parent --template "{rev}"') do (
 echo %REV%
 
 set PATHS=
-for /f "tokens=2 delims=/" %%i in ('findstr /r "<Path>res_mods" C:\games\World_of_Warships\paths.xml') do (
+for /f "tokens=2 delims=/" %%i in ('findstr /r "<Path>res_mods" %GAME_FOLDER%\paths.xml') do (
   set PATHS=%%i
 )
 SET VERS=%PATHS:~0,-1%
@@ -20,4 +61,4 @@ mkdir %VERS%
 cd %VERS%
 xcopy ..\..\out\gui gui /i /e
 cd ..
-"C:\Program Files\7-Zip\7z.exe" a -r C:\src\owncloud\mxcamo\mxcamo-%VERS%-%REV%.zip %VER%
+"C:\Program Files\7-Zip\7z.exe" a -r %DESTINATION%\mxcamo-%VERS%-%REV%.zip %VER%
